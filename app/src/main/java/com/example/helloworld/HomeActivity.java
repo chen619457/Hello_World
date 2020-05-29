7cd20d1beee6c8c4a891ab5fc304aa8a9acf0dfc
陈少坤 <1175275831@qq.com>
2020-05-29 11:06:45 +0800

    登录功能：将用户资料显示到Home界面

--- app/src/main/java/com/example/helloworld/HomeActivity.java
+++ app/src/main/java/com/example/helloworld/HomeActivity.java
@@ -3,6 +3,7 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import com.example.helloworld.databinding.ActivityHomeBinding;
@@ -18,7 +19,16 @@ public class HomeActivity extends AppCompatActivity {
 
         Intent intent=getIntent();
         String phone = intent.getStringExtra("data_phone");
-        mBinding.textPhone.setText(phone);
+        mBinding.userPhone.setText(phone);
+
+        SharedPreferences sp = getSharedPreferences("user_info",MODE_PRIVATE);
+        String userName =sp.getString("name"+phone,"0");
+        String userSex =sp.getString("sex"+phone,"0");
+        String userSms =sp.getString("sms"+phone,"0").equals("1")?"接受":"不接受";
+        mBinding.userName.setText(userName);
+        mBinding.userSex.setText(userSex);
+        String temp=mBinding.userSms.getText().toString()+":"+userSms;
+        mBinding.userSms.setText(temp);
 
     }
 }
