ffbd687772a3c50ab639439e0a46b655cb683b7c
陈少坤 <1175275831@qq.com>
2020-06-12 09:52:21 +0800

    HomeActivity.java:新增actionStart()静态方法，用于启动Activity MainActivity.java:改变启动HomeActivity界面的方式

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -34,17 +34,20 @@ public class MainActivity extends AppCompatActivity {
                 String tempPwd=sp.getString("pwd"+phone,"error");
                if (phone.equals(tempPhone)&&pwd.equals(tempPwd))
                {
-                   Bundle bundle=new Bundle();
+//                   Bundle bundle=new Bundle();
                    String userName =sp.getString("name"+phone,"0");
                    String userSex =sp.getString("sex"+phone,"0");
                    String userSms ="1".equals(sp.getString("sms"+phone,"0")) ?"接受":"不接受";
-                   UserInfo u = new UserInfo(userName,pwd,userSex,phone,userSms);
-                   bundle.putSerializable("userInfo",u);
-                   Intent intent=new Intent(MainActivity.this,HomeActivity.class);
-                   intent.putExtras(bundle);
+
+//                   UserInfo u = new UserInfo(userName,pwd,userSex,phone,userSms);
+//                   bundle.putSerializable("userInfo",u);
+//                   Intent intent=new Intent(MainActivity.this,HomeActivity.class);
+//                   intent.putExtras(bundle);
                    //intent.putExtra("data_phone",phone);
                   // startActivity(intent);
-                   startActivityForResult(intent,RESULT_CODE);
+//                   startActivityForResult(intent,RESULT_CODE);
+                   HomeActivity.actionStart(MainActivity.this,userName,pwd,userSex,phone,userSms,RESULT_CODE);
+
                }
                else {
                    Toast.makeText(MainActivity.this,"手机号码或密码错误",Toast.LENGTH_LONG).show();
