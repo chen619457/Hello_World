4a456f693357a290a318b01e891f70cc9db9acdb
陈少坤 <1175275831@qq.com>
2020-06-12 17:48:15 +0800

    新增两个Activity：ResetPwdActivity和UpdatePwdActivity 完成了两个Activity的布局，并增加了相应的图片和字符串资源

--- app/src/main/java/com/example/helloworld/ResetPwdActivity2.java
+++ app/src/main/java/com/example/helloworld/ResetPwdActivity2.java
@@ -3,12 +3,27 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.helloworld.databinding.ActivityHomeBinding;
+import com.example.helloworld.databinding.ActivityResetPwdBinding;
 
 public class ResetPwdActivity2 extends AppCompatActivity {
+    private ActivityResetPwdBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_reset_pwd);
+        mBinding=ActivityResetPwdBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
+
+        mBinding.buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/helloworld/UpdatePwdActivity.java
+++ app/src/main/java/com/example/helloworld/UpdatePwdActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.helloworld.databinding.ActivityResetPwdBinding;
+import com.example.helloworld.databinding.ActivityUpdatePwdBinding;
+
 public class UpdatePwdActivity extends AppCompatActivity {
+    private ActivityUpdatePwdBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_pwd);
+        mBinding=ActivityUpdatePwdBinding.inflate(getLayoutInflater());
+        setContentView(mBinding.getRoot());
     }
 }
